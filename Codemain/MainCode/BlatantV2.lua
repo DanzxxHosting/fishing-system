9d17bd72fdfe373589d79cb7b022e9e777592054
DanzxxHosting <ddefann402@gmail.com>
2025-12-22 00:18:36 +0700

    Update BlatantV2.lua

--- Codemain/MainCode/BlatantV2.lua
+++ Codemain/MainCode/BlatantV2.lua
@@ -19,10 +19,10 @@ local fishing = {
     CurrentCycle = 0,
     TotalFish = 0,
     Settings = {
-        FishingDelay = 0.13,      -- Dikurangi dari 0.3 ke 0.05
-        CancelDelay = 0.10,       -- Dikurangi dari 0.05 ke 0.01
-        HookWaitTime = 0.10,      -- Dikurangi dari 0.30 ke 0.15
-        CastDelay = 0.15,         -- Dikurangi dari 0.07 ke 0.03
+        FishingDelay = 0.20,      -- Dikurangi dari 0.3 ke 0.05
+        CancelDelay = 0.15,       -- Dikurangi dari 0.05 ke 0.01
+        HookWaitTime = 0.13,      -- Dikurangi dari 0.30 ke 0.15
+        CastDelay = 0.20,         -- Dikurangi dari 0.07 ke 0.03
         TimeoutDelay = 0.5,       -- Dikurangi dari 1.1 ke 0.8
     },
 }
