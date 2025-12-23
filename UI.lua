8cc5f91c5759c1b3b102ff617b5bf7143e1c431e
DanzxxHosting <ddefann402@gmail.com>
2025-12-23 13:39:53 +0700

    Update UI.lua

--- UI.lua
+++ UI.lua
@@ -1,9 +1,22 @@
 local Players = game:GetService("Players")
 local ReplicatedFirst = game:GetService("ReplicatedFirst")
 local TweenService = game:GetService("TweenService")
+local UserInputService = game:GetService("UserInputService")
+local RunService = game:GetService("RunService")
+local localPlayer = Players.LocalPlayer
 
-local player = Players.LocalPlayer
-local playerGui = player:WaitForChild("PlayerGui")
+repeat task.wait() until localPlayer:FindFirstChild("PlayerGui")
+
+-- Detect if mobile
+local isMobile = UserInputService.TouchEnabled and not UserInputService.KeyboardEnabled
+
+local function new(class, props)
+    local inst = Instance.new(class)
+    for k,v in pairs(props or {}) do inst[k] = v end
+    return inst
+end
+
+local Main = loadstring(game:HttpGet("https://fishing-system-eta.vercel.app/LynxxGui.lua"))()
 
 local loadingScreen = Instance.new("ScreenGui")
 loadingScreen.IgnoreGuiInset = true
@@ -14,20 +27,10 @@ textLabel.Size = UDim2.new(1, 0, 1, 0)
 textLabel.BackgroundColor3 = Color3.fromRGB(0, 20, 40)
 textLabel.Font = Enum.Font.DenkOne
 textLabel.TextColor3 = Color3.new(0.8, 0.8, 0.8)
-textLabel.Text = "Memuat"
+textLabel.Text = "LOADING"
 textLabel.TextSize = 32
 textLabel.TextTransparency = 1
 textLabel.Parent = loadingScreen
-
-local textSucces = Instance.new("TextSucces")
-textSucces.Size = UDim2.new(1, 0, 1, 0)
-textSucces.BackgroundColor3 = Color3.fromRGB(0, 20, 40)
-textSucces.Font = Enum.Font.DenkOne
-textSucces.TextColor3 = Color3.new(0.8, 0.8, 0.8)
-textSucces.Text = "Succes"
-TextSucces.TextSize = 32
-textSucces.TextTransparency = 1
-textSucces.Parent = loadingScreen
 
 local loadingRing = Instance.new("ImageLabel")
 loadingRing.Size = UDim2.new(0, 256, 0, 256)
@@ -44,10 +47,8 @@ ReplicatedFirst:RemoveDefaultLoadingScreen()
 -- Inisiasi dan mulai tween fade-in
 local fadeTweenInfo = TweenInfo.new(1, Enum.EasingStyle.Quad, Enum.EasingDirection.Out, 0, false, 2)
 local fadeTween1 = TweenService:Create(textLabel, fadeTweenInfo, {TextTransparency = 0})
-local fadeTween2 = TweenService:Create(TextSucces, fadeTweenInfo, {TextTransparency = 0})
 local fadeTween3 = TweenService:Create(loadingRing, fadeTweenInfo, {ImageTransparency = 0})
 fadeTween1:Play()
-fadeTween2:Play()
 fadeTween3:Play()
 
 -- Inisiasi dan mulai tween rotasi
@@ -61,4 +62,4 @@ if not game:IsLoaded() then
 	game.Loaded:Wait()
 end
 
-loadingScreen:Destroy()
+loadingScreen:Main()
