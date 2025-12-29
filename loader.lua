f987ef37fa264383c109d2cb09e792c70740ff89
DanzxxHosting <ddefann402@gmail.com>
2025-12-29 22:45:49 +0700

    Add files via upload

---
+++ loader.lua
@@ -0,0 +1,9 @@
+-- Load both modules
+loadstring(game:HttpGet("https://raw.githubusercontent.com/DanzxxHosting/fishing-system/refs/heads/main/fitur.lua"))()
+
+local guiSource = game:HttpGet("https://raw.githubusercontent.com/DanzxxHosting/fishing-system/refs/heads/main/LynxxGui.lua")
+loadstring(guiSource)()
+
+print("🎣 Game Fisher v3.0 + Features loaded!")
+print("Features: ESP, Auto-Farm, Remote Exploiter, Anti-AFK, Script Dumper")
+print("GUI: F4 to toggle, F5 to refresh")
